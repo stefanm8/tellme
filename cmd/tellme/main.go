19687d727d4dd0977a74855089c11bf82d90eb1f
stefanm8 <stefanmihartescu@gmail.com>
2022-10-16 15:35:46 +0300

    initial commit

---
+++ cmd/tellme/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/urfave/cli/v2"
+)
+
+type Choice struct {
+	Text string `json:"text"`
+}
+type OpenAIResponse struct {
+	Choices []Choice `json:"choices"`
+}
+
+var openaiURI = "https://api.openai.com/v1/completions"
+
+var prompt = `\nConvert this text to a linux command\n\ntext: `
+var endPrompt = `\ncommand: `
+
+var client = &http.Client{}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readRequestBody(req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+}
+
+func parseOpenAIResponse(resp *http.Response) (*OpenAIResponse, error) {
+	ai := &OpenAIResponse{}
+	err := json.NewDecoder(resp.Body).Decode(ai)
+	return ai, err
+}
+
+func fzf(choices []string) {
+	idx, err := fuzzyfinder.Find(
+		choices,
+		func(i int) string {
+			return choices[i]
+		},
+		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+			return choices[i]
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v\n", strings.Trim(choices[idx], "\n"))
+
+}
+
+func uniqueValuesFromList(list []Choice) []string {
+	uniqueValues := make(map[string]bool)
+	lst := []string{}
+	for _, item := range list {
+		uniqueValues[item.Text] = true
+	}
+
+	for key := range uniqueValues {
+		lst = append(lst, strings.Trim(strings.Trim(key, "\n"), " "))
+	}
+
+	return lst
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "tellme",
+		Usage: "text to command",
+		Action: func(ctx *cli.Context) error {
+
+			if len(ctx.Args().Slice()) == 0 {
+				fmt.Println("Please provide a text to convert to a command")
+				os.Exit(1)
+			}
+			text := strings.Join(ctx.Args().Slice(), " ")
+
+			body := []byte(`{
+				"model": "text-davinci-002",
+				"prompt": "` + prompt + text + endPrompt + `",
+				"frequency_penalty": 0.8,
+				"temperature": 0.8,
+				"top_p": 1,
+				"n": 5,
+				"max_tokens": 250
+			}`)
+
+			req, err := http.NewRequest("POST", openaiURI, bytes.NewBuffer(body))
+			checkError(err)
+
+			// readRequestBody(req)
+			req.Header.Add("Authorization", "Bearer "+os.Getenv("TELLME_OPENAI_TOKEN"))
+			req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+			res, err := client.Do(req)
+			checkError(err)
+
+			aires, err := parseOpenAIResponse(res)
+			checkError(err)
+
+			lst := uniqueValuesFromList(aires.Choices)
+			fzf(lst)
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/stefanm8/tellme
+
+go 1.19
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.4.0 // indirect
+	github.com/ktr0731/go-fuzzyfinder v0.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/nsf/termbox-go v0.0.0-20201124104050-ed494de23a00 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.19.2 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
